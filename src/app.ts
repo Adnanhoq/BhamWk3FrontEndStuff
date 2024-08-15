4d42e811474031e547e7b0b000ed4ab3fc4ce5be
mohammed <mohammedadnan.hoq@kainos.com>
2024-08-15 12:19:52 +0100

    Fixed Accessibility

--- src/app.ts
+++ src/app.ts
@@ -30,7 +30,7 @@ app.listen(3000, () => {
     console.log('Server started on port 3000');
 });
 
-app.get('/', getAllDatabases);
+// app.get('/', getAllDatabases);
 
 app.get('/loginForm', getLoginForm);
 app.post('/loginForm', postLoginForm);
